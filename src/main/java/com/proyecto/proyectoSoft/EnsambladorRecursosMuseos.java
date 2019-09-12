bb48bd88d6aa0c14cc4a31e5a68bfafe99c1ffdc
Adam Hans Quiroz Castillo <adam123quiroz@gmail.com>
2019-09-12 17:04:54 -0400

    arreglando errores

--- src/main/java/com/proyecto/proyectoSoft/EnsambladorRecursosMuseos.java
+++ src/main/java/com/proyecto/proyectoSoft/EnsambladorRecursosMuseos.java
@@ -4,12 +4,14 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EnsambladorRecursosMuseos implements ResourceAssembler<Museo, Resource<Museo>> {
     @Override
     public Resource<Museo> toResource(Museo museo) {
         return new Resource<>(museo,
-                linkTo(methodOn(MuseoControlador.class).one(museo.getId())).withSelfRel(),
+                linkTo(methodOn(MuseoControlador.class).obtenerMuseoPorId(museo.getId())).withSelfRel(),
                 linkTo(methodOn(MuseoControlador.class).all()).withRel("museos"));
     }
 }
