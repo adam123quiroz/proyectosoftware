25134a2da803a8d4147afb9bf85f117ce37d2d45
Adam Hans Quiroz Castillo <adam123quiroz@gmail.com>
2019-09-12 10:32:12 -0400

    Initial commit

---
+++ src/main/java/com/proyecto/proyectoSoft/ProyectoSoftApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyectoSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSoftApplication.class, args);
+	}
+
+}
